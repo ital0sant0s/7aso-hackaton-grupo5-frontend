35c6e666887e0ed48bd5667b3ae7581028ffcae7
Lucas Barros <lucas2019.fullstack@gmail.com>
2022-07-02 14:00:35 -0300

    one signal monitoring

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Appsignal from "@appsignal/javascript"
+const appsignal = new Appsignal({ key: process.env.REACT_APP_ONESIGNAL_KEY })
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +18,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+appsignal.demo();
